481499860e9d2d47c1039a6e10504124e4dd500d
Cedric Wu <cedric.wu@chinapex.com>
2017-08-25 17:52:32 +0800

    新增功能

--- notification.go
+++ notification.go
@@ -19,8 +19,8 @@ const (
 )
 
 type notifier interface {
-	push(title string, text string, iconPath string) *exec.Cmd
-	pushCritical(title string, text string, iconPath string) *exec.Cmd
+	push(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd
+	pushCritical(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd
 }
 
 type Notificator struct {
@@ -28,7 +28,7 @@ type Notificator struct {
 	defaultIcon string
 }
 
-func (n Notificator) Push(title string, text string, iconPath string, urgency string) error {
+func (n Notificator) Push(title string, text string, timeout int, soundPath string, clickURL string, iconPath string, urgency string) error {
 	icon := n.defaultIcon
 
 	if iconPath != "" {
@@ -36,10 +36,10 @@ func (n Notificator) Push(title string, text string, iconPath string, urgency st
 	}
 
 	if urgency == UR_CRITICAL {
-		return n.notifier.pushCritical(title, text, icon).Run()
+		return n.notifier.pushCritical(title, text, timeout, soundPath, clickURL, icon).Run()
 	}
 
-	return n.notifier.push(title, text, icon).Run()
+	return n.notifier.push(title, text, timeout, soundPath, clickURL, icon).Run()
 
 }
 
@@ -47,7 +47,7 @@ type osxNotificator struct {
 	AppName string
 }
 
-func (o osxNotificator) push(title string, text string, iconPath string) *exec.Cmd {
+func (o osxNotificator) push(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd {
 
 	// Checks if terminal-notifier exists, and is accessible.
 
@@ -70,7 +70,7 @@ func (o osxNotificator) push(title string, text string, iconPath string) *exec.C
 }
 
 // Causes the notification to stick around until clicked.
-func (o osxNotificator) pushCritical(title string, text string, iconPath string) *exec.Cmd {
+func (o osxNotificator) pushCritical(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd {
 
 	// same function as above...
 
@@ -79,7 +79,7 @@ func (o osxNotificator) pushCritical(title string, text string, iconPath string)
 
 	if term_notif == true {
 		// timeout set to 30 seconds, to show the importance of the notification
-		return exec.Command("terminal-notifier", "-title", o.AppName, "-message", text, "-subtitle", title, "-timeout", "30")
+		return exec.Command("terminal-notifier", "-title", o.AppName, "-message", text, "-subtitle", title, "-timeout", strconv.Itoa(timeout), "-open", clickURL, "-sound", soundPath)
 	} else if os_version_check == true {
 		notification := fmt.Sprintf("display notification \"%s\" with title \"%s\" subtitle \"%s\"", text, o.AppName, title)
 		return exec.Command("osascript", "-e", notification)
@@ -91,23 +91,23 @@ func (o osxNotificator) pushCritical(title string, text string, iconPath string)
 
 type linuxNotificator struct{}
 
-func (l linuxNotificator) push(title string, text string, iconPath string) *exec.Cmd {
+func (l linuxNotificator) push(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd {
 	return exec.Command("notify-send", "-i", iconPath, title, text)
 }
 
 // Causes the notification to stick around until clicked.
-func (l linuxNotificator) pushCritical(title string, text string, iconPath string) *exec.Cmd {
+func (l linuxNotificator) pushCritical(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd {
 	return exec.Command("notify-send", "-i", iconPath, title, text, "-u", "critical")
 }
 
 type windowsNotificator struct{}
 
-func (w windowsNotificator) push(title string, text string, iconPath string) *exec.Cmd {
+func (w windowsNotificator) push(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd {
 	return exec.Command("growlnotify", "/i:", iconPath, "/t:", title, text)
 }
 
 // Causes the notification to stick around until clicked.
-func (w windowsNotificator) pushCritical(title string, text string, iconPath string) *exec.Cmd {
+func (w windowsNotificator) pushCritical(title string, text string, timeout int, soundPath string, clickURL string, iconPath string) *exec.Cmd {
 	return exec.Command("notify-send", "-i", iconPath, title, text, "/s", "true", "/p", "2")
 }
 
